a8283df5dbce313ce312890843d7fdd7db2f93b7
attckoneren <godsj1342@gmail.com>
2024-01-09 19:46:28 +0900

    some repairs

--- src/db.js
+++ src/db.js
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.DB_URL);
+const dbUsername = process.env.DB_USERNAME;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+const dbPort = process.env.DB_PORT;
+const dbName = process.env.DB_NAME;
+
+const dbURL = `mongodb://${dbUsername}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`;
+
+mongoose.connect(dbURL);
 
 const db = mongoose.connection;
 
